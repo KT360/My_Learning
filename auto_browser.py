40c4a6318ddbf133989ca05d145c3edf870d4f9a
KT360 <31077743+KT360@users.noreply.github.com>
2023-05-17 09:51:29 -0400

    Update auto_browser.py

--- auto_browser.py
+++ auto_browser.py
@@ -15,16 +15,62 @@ driver.get('https://app.powerbi.com/')
 email = 'akamto@txt.textron.com'
 password = 'Tx972002'
 
-time.sleep(5)
+time.sleep(2)
 
 # Locate the email input field and enter your email
-email_input = driver.find_element(By.NAME,"'loginfmt'")
+email_input = driver.find_element(By.XPATH,"//*[@id='email']")
 email_input.send_keys(email)
 email_input.send_keys(Keys.RETURN)
 
 # Wait for the password input field to load
 time.sleep(2)
 
+##driver.get("https://login.textron.com/idp/cDnwUUXuEu/resume/idp/prp.ping")
+
+driver.get("https://app.powerbi.com/home")
+
+time.sleep(3)
+
+"""
+newPass = driver.find_element(By.ID, "password")
+newPass.send_keys(password)
+newPass.send_keys(Keys.RETURN)
+"""
+
+
+browse = driver.find_element(By.XPATH, "//*[@id='leftNavPane']/div/div/tri-navbar-label-item[3]/button")
+browse.click()
+
+time.sleep(2)
+
+lpaLink = driver.find_element(By.XPATH, "//*[@id='artifactContentView']/div[1]/div[1]/div[2]/span/a")
+lpaLink.click()
+
+time.sleep(10)
+
+lap1Tab = driver.find_element(By.XPATH, "//*[@id='content']/tri-shell/tri-extension-page-outlet/div[2]/report/exploration-container/div/div/docking-container/div/div/exploration-fluent-navigation/section/mat-action-list/button[3]")
+lap1Tab.click()
+
+time.sleep(2)
+
+visual = driver.find_element(By.XPATH, "//*[@id='pvExplorationHost']/div/div/exploration/div/explore-canvas/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container[3]/transform/div/div[2]/div/div")
+visual.click()
+
+options = driver.find_element(By.XPATH, "//*[@id='pvExplorationHost']/div/div/exploration/div/explore-canvas/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container[3]/transform/div/visual-container-header/div/div/div/visual-container-options-menu/visual-header-item-container/div/button")
+options.click()
+
+exportPane = driver.find_element(By.XPATH, "//*[@id='5']")
+exportPane.click()
+
+time.sleep(2)
+
+export = driver.find_element(By.XPATH, "//button[text()='Export']")
+export.click()
+
+time.sleep(10)
+
+##driver.get("http://{email}:{password}@https://login.textron.com/idp/16Z7c1Ib1w/resume/idp/prp.ping")
+"""
 # Locate the password input field and enter your password
 password_input = driver.find_element_by_name('passwd')
 password_input.send_keys(password)
@@ -55,7 +101,7 @@ export = driver.find_element_by_xpath("//*[@id='5']")
 export.click()
 
 download = driver.find_element_by_xpath("//*[@id='mat-dialog-0']/export-data-dialog/mat-dialog-actions/button[1]")
-download.click()
+download.click()"""
 # Replace 'Slicer_Name' with the name of the slicer you want to interact with
 """slicer_name = 'Slicer_Name'
 
@@ -97,5 +143,6 @@ export_data_option = WebDriverWait(driver, 10).until(
 
 # Click on the "Export data" option
 export_data_option.click()"""
+
 
 # The data export process will start, and the Excel file will be downloaded
